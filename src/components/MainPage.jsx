b2af8f5067d6f4538a8a3c8a04fb7cecd2278a57
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2023-02-04 03:07:05 -0800

    framework completed

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -4,11 +4,51 @@ import { useState } from 'react';
 import "../components/MainPage.jsx";
 
 import { Button } from '@mui/material';
+import { styled } from '@mui/material';
 import { TextField } from '@mui/material';
 
 import { useNavigate } from 'react-router-dom';
 
 import "../App.css";
+
+
+const StyledField = styled ( TextField ) ( {
+  // color of the text being entered
+  '& input': {
+      color: 'black',
+  },
+
+  // colour of the text when not focused
+  '& label': {
+      color: '#000000',
+  },
+
+  // colour of the text that moves to the top
+  '& label.Mui-focused': {
+    color: 'black',
+  },
+
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'black',
+  },
+
+  '& .MuiOutlinedInput-root': {
+      // outline colour when nothing is happening
+    '& fieldset': {
+      borderColor: 'black',
+    },
+      // outline colour when hovering
+    '&:hover fieldset': {
+      borderColor: 'red',
+    },
+    // outline colour when focused
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+  },
+});
+
+
 
 function MainPage() {
   const nav = useNavigate();
@@ -27,7 +67,7 @@ function MainPage() {
           <p className='PointsLabel'>Enter the amount of slug points you have:</p>
 
           <div className='s3'>
-              <TextField
+              <StyledField
                 onChange={ (event) => { SetAmount( event.target.value ) } }
                 className="text"
                 id="outlined-basic"
@@ -36,7 +76,7 @@ function MainPage() {
                 style={ { color: "#EEC643" } }
                 sx={ { margin: 1 } }/>
               
-              <button onClick={ () => console.log( Amount ) }>print</button>
+              {/* <button onClick={ () => console.log( Amount ) }>print</button> */}
               
               <Button onClick={ () => nav( "results", { state: Amount } ) } variant='contained' className='MUIButton' style={{ backgroundColor: "#0D21A1", fontFamily: "Roboto", margin: 0 }}>Calculate</Button>
           </div>
--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+import { Stack } from '@mui/material';
+import { Button } from '@mui/material';
 import { AppBar } from '@mui/material';
 import { Toolbar } from '@mui/material';
 import { IconButton } from '@mui/material';
 import { Typography } from '@mui/material';
 
-import { useLocation, useNavigate } from 'react-router-dom';
-
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { createTheme } from '@mui/material';
 import { ThemeProvider } from '@mui/material';
 
 import './Results.css';
+
+
 
 function UpperBar() {
     const nav = useNavigate();
@@ -45,15 +50,28 @@ const darkTheme = createTheme({
   });
 
 function Results( props ) {
+    const nav = useNavigate();
     const location = useLocation();
     console.log( location.state )
 
     var amount = 0;
+    var CanProceed = false
 
     if( !isNaN( location.state ) ) {
-        amount = location.state
+        if( location.state !== null && location.state !== "" ) {
+            amount = location.state
+            CanProceed = true
+        } else {
+            amount = "No number entered"
+        }
     } else {
-        amount = "Not a number"
+        var a = String( location.state );
+        if( a.includes( "," ) ) {
+            amount = "Please do not use commas"
+        } else {
+            amount = "Not a number"
+        }
+
     }
 
     return (
@@ -61,7 +79,10 @@ function Results( props ) {
             <UpperBar/>
 
             <div className='container'>
-                <p className='text'>{ amount }</p>
+                <Stack spacing={ 1 }>
+                    <p className='text'>{ amount }</p>
+                    { CanProceed ? <></> : <Button variant='contained' style={{ color: "black", backgroundColor: "#EEC643", fontFamily: "Roboto", margin: 0 }} startIcon={ <ArrowBackIcon/> } onClick={ () => nav( "/" ) }>Go back</Button> }
+                </Stack>
             </div>
             
         </ThemeProvider>
