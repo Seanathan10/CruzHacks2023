bea3a512b74887c8a02a94df0c34103d50073c50
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2023-02-04 01:21:32 -0800

    rescue point

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { Button } from '@mui/material';
+import { TextField } from '@mui/material';
 
 function App() {
+
   return (
-    <div className="App">
-      <div className='App2'>
-        Hello
+    <div className='background'>
+      <div className="container">
+
+        <div className='title'>
+          <p className='title_text'>Slug Points Budgeting</p>
+        </div>
+
+
+        <div className='content'>
+          <p className='PointsLabel'>Enter the amount of slug points you have:</p>
+
+          <div className='s3'>
+              <TextField className="text" id="outlined-basic" label="Outlined" variant="outlined" style={ { color: "#EEC643" } } sx={ { margin: 1 } }/>
+              <Button onClick={ alert } variant='contained' className='MUIButton' style={{ backgroundColor: "#0D21A1", fontFamily: "Roboto", margin: 0 }}>Calculate</Button>
+          </div>
+        </div> 
       </div>
-
-      <button>Test Standard</button>
-
-      <Button variant='contained' className='MUIButton' style={{ backgroundColor: "red", fontFamily: "Comic Sans MS" }}>MUI Test</Button>
     </div>
   );
 }
